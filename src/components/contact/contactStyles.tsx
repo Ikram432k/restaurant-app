44d40443994626b7d62889d42e89e08843286ced
ikram mohideen <ikram432k@gmail.com>
2022-06-13 21:23:38 +0530

    src/components/contact/contactStyles.tsx

---
+++ src/components/contact/contactStyles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export const Div = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding:3rem;
+background-color: rgba(0, 0, 0, 0.6);
+flex-grow:1;
+`;
