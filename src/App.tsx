abdff4eb5433544601d7da6bf573d257e4f7fdb2
ikram mohideen <ikram432k@gmail.com>
2022-06-13 21:20:06 +0530

    src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { createGlobalStyle } from 'styled-components';
 import img from './components/assets/backgound.webp';
 import Nav from './components/navBar/navBar'
 import Home from './components/home/home';
-import Menu from './components/menu/menu'
+import Menu from './components/menu/menu';
+import Feedback from './components/feedback/feedback';
+import Contact from './components/contact/contact';
 const GlobalStyle = createGlobalStyle`
  body,h1,h2,h3,p {
   margin: 0;
@@ -25,8 +27,11 @@ function App() {
     <GlobalStyle />
     <Nav/>
     <Routes>
-      <Route path='/home' element={<Home/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/menu' element={<Menu/>}/>
+      <Route path='/feedback' element={<Feedback/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+
     </Routes>
     </Router>  );
 }
