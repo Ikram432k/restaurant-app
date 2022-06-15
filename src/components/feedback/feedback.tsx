67a1903aa8579886ab580eaf8ca4bf1af995e437
ikram mohideen <ikram432k@gmail.com>
2022-06-15 20:44:37 +0530

    src/components/feedback/feedback.tsx

--- src/components/feedback/feedback.tsx
+++ src/components/feedback/feedback.tsx
@@ -1,24 +1,36 @@
 import { async } from "@firebase/util";
 import { useEffect, useState } from "react";
-import { getReview } from "../backend/backend"
+import { getReview,observer,signOuts,signIn } from "../backend/backend";
 import { Div  } from "./feedbackStyle";
 const Feedback =()=>{
     const [review,setReview] = useState<any[]>([])
+    const [user,setUserin] = useState<any>()
+
     useEffect(()=>{
         getReviewList();
+        observer();
+
     },[])
 
     const getReviewList = async()=>{
         const data = await getReview();
         setReview(data);
     }
+    const getuserinfo =()=>{
+        const signindetail =  signIn();
+        setUserin(signindetail);
+        console.log(user);
+    }
 
     return(
         <Div>
             <div className="add">
                 <h2>Add Feedback</h2>
-                <p>if your already visited our restaurunt kindly add your expirence at los pollos hermanos by logging in.</p>
-                <button>log in</button>
+                <p>if your already visited our restaurunt kindly add your expirence at los pollos hermanos.</p>
+                <button onClick={getuserinfo}>Add</button>
+                <button onClick={signOuts}>leave</button>
+                {/*   <p>{user}</p> */}
+
             </div>
             <div className="reviewList">
             <h2>Feedback from our customers</h2>
