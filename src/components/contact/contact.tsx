98163d23469ec49b89f9724842aeca41adff6ad0
ikram mohideen <ikram432k@gmail.com>
2022-06-15 20:43:58 +0530

    src/components/contact/contact.tsx

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -3,15 +3,24 @@ import { Div } from "./contactStyles";
 const Contact =()=>{
     return(
         <Div>
-            <div className="details">
-                <p>Phone 505-246-6200</p>
-                <p>Mail us at los_pollos_hermanos@gmail.com </p>
+            <div className="whole">
+                <div className="details">
+                <div className="icons">
+                    <i className="material-icons">call</i><p>505-246-6200</p>
+                </div>
+                <div className="icons">
+                    <i className="material-icons">email</i><p> los_pollos_hermanos@gmail.com </p>
+                </div>
+                <div className="icons">
+                    <i className="material-icons">room</i><p>Albuquerque, New Mexico, United States</p>
+                </div>
             </div>
             <div className="about">
                 <img src={img} alt="gus fring"/>
-                <p>
-                    Hi i am Gus fring .We opened our first Restaurunt at Albuquerque in 1989 ,now we maintain more than 20 restaurunt at 14 different locations. here at our restaurunt The finest ingredients are brought together with love and care, then slow cooked to perfection. Yes, the old ways are still best at Los Pollos Hermanos. But don't take my word for it. One taste, and you'll know. 
+                <p className="para">
+                    Hi i am Gustavo fring .We opened our first Restaurunt at Albuquerque in 1989 ,now we maintain more than 20 restaurunt at 14 different locations. here at our restaurunt The finest ingredients are brought together with love and care, then slow cooked to perfection. Yes, the old ways are still best at Los Pollos Hermanos. But don't take my word for it. One taste, and you'll know.
                 </p>
+            </div>
             </div>
         </Div>
     )
