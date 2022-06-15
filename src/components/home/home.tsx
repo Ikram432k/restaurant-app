021096f72ec436df6f52f3d4aa5027079fb8755f
ikram mohideen <ikram432k@gmail.com>
2022-06-15 20:44:59 +0530

    src/components/home/home.tsx

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -9,19 +9,16 @@ const Home =()=>{
     return(
         <Div>
             <Contents>           
-            <img src={img} alt="background_img"/>
-            <div>
-            <h3>
-                Welcome to Los Pollos Hermanos. 
-            </h3>
-            <p>
-            Los Pollos Hermanos was a fast-food restaurant chain that specialized in fried chicken, operating across the southwestern United States. The name, pronounced [los ˈpo.ʝos eɾˈma.nos], is ungrammatical Spanish for "The Chicken Brothers." Founded by Gustavo Fring and Max Arciniega, the restaurant chain had fourteen locations throughout the southwest and check our special recipies at Menu section above. 
-            </p>
-            </div>
+                  <img src={img} alt="background_img"/>
+                <div>
+                    <h3>
+                    Welcome to Los Pollos Hermanos. 
+                    </h3>
+                    <p>
+                    Los Pollos Hermanos was a fast-food restaurant chain that specialized in fried chicken, operating across the southwestern United States. The name is ungrammatical Spanish for "The Chicken Brothers." Founded by Gustavo Fring and Max Arciniega, the restaurant chain had fourteen locations throughout the southwest and check our special recipies at Menu section above. 
+                    </p>
+                </div>
             </Contents>
-            <p>
-
-            </p>
         </Div>
     )
 }
